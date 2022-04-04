d4c31a830ca0b3bce4cb702601ee758c7e3207c0
jamessacummins <jamessacummins@gmail.com>
2022-04-04 11:31:56 +1000

    Adding in optional print

--- MyClient.java
+++ MyClient.java
@@ -2,11 +2,9 @@ import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
 
-
 class MyClient {
 
     // state variables
-
 
     public ArrayList<String> serverMessageList = new ArrayList<String>();
     public ArrayList<Server> allServersList;
@@ -18,120 +16,132 @@ class MyClient {
     public int numberOfMessages;
     public Job currentJob;
     public Server selectedServer;
+    public Boolean printAllMessages = false;
 
     public static void main(String[] args) {
-        
+
         // instantiating a copy of a Client class so variables can be "non-static"
         MyClient nonStaticClient = new MyClient();
         nonStaticClient.run(args);
-        
+
     };
 
-    public void run(String[] args){
-        System.out.println("Client is up and running!"); 
+    public void run(String[] args) {
+        print("Client is up and running!");
 
-        try{
+        try {
 
             initialise(args);
             connect();
-            while(true){
+            while (true) {
                 writeThenRead("REDY");
-                if(getLatestMessage().equals("NONE") ) {
+                if (getLatestMessage().equals("NONE")) {
                     break;
-                } else if (getLatestMessage().contains("JCPL")){
+                } else if (getLatestMessage().contains("JCPL")) {
                     continue;
-                } else if (getLatestMessage().contains("JOBN")){
+                } else if (getLatestMessage().contains("JOBN")) {
                     getAndScheduleJobToNextLargestServer();
                     continue;
                 } else {
                     break;
                 }
-            };
+            }
+            ;
 
-            
             writeThenRead("QUIT");
             dataOutputStream.close();
             reader.close();
             socket.close();
 
         }
-        
-        catch(Exception e) {
+
+        catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public void initialise(String[] args){
-        try{
-            int port;
+    public void initialise(String[] args) {
+        try {
+            int port = 50000;
 
-            // if a command variable is provided for port use that else use 6666
-            if(args.length == 1){
-                port = Integer.parseInt(args[0]);
-                System.out.println("Running on port " + port);
-            } else { port = 6666; };
-            
-            socket = new Socket("localhost", 50000);
-            
+            // if first command variable is provided for port use that else use 50000
+            // if either variable 1 or 2 is "v" use verbose mode (print everything)
+            if (args.length >= 1) {
+                if (args[0].equals("v")) {
+                    printAllMessages = true;
+                } else {
+                    port = Integer.parseInt(args[0]);
+                    print("Running on port " + port);
+                }
+                ;
+                if (args.length >= 2) {
+                    if (args[1].equals("v"))
+                        printAllMessages = true;
+                }
+            };
+
+            socket = new Socket("localhost", port);
+
             dataOutputStream = new DataOutputStream(socket.getOutputStream());
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     };
 
-    public void connect(){
+    public void connect() {
         writeThenRead("HELO");
         writeThenRead("AUTH " + System.getProperty("user.name"));
     };
 
-    public void getAndScheduleJobToNextLargestServer(){
+    public void getAndScheduleJobToNextLargestServer() {
         updateCurrentJob();
         writeThenRead("GETS All");
         writeThenRead("OK");
-        if(largestServersList == null){
+        if (largestServersList == null) {
             updateAllServersList();
             findLargestTypeThenUpdateLargestServersList();
-        };
+        }
+        ;
         selectedServer = getCurrentLargestServer();
-        System.out.println("Current largest server is " + selectedServer.serverType + " " + selectedServer.serverID);
+        print("Current largest server is " + selectedServer.serverType + " " + selectedServer.serverID);
         writeThenRead("OK");
         writeThenRead("SCHD " + currentJob.jobID + " " + selectedServer.serverType + " " + selectedServer.serverID);
     };
 
-    public void findLargestTypeThenUpdateLargestServersList(){
+    public void findLargestTypeThenUpdateLargestServersList() {
         String largestType = "";
         int largestCoreSize = 0;
-        for(Server server: allServersList){
-            if(server.core > largestCoreSize){
+        for (Server server : allServersList) {
+            if (server.core > largestCoreSize) {
                 largestType = server.serverType;
                 largestCoreSize = server.core;
             }
         }
         largestServersList = new ArrayList<Server>();
-        for(Server server: allServersList){
-            if(server.serverType.equals(largestType)){
+        for (Server server : allServersList) {
+            if (server.serverType.equals(largestType)) {
                 largestServersList.add(server);
             }
         }
-        System.out.println("Largest server type is " + largestType + ", there are " + largestServersList.size() + " " + largestType + " servers.");
+        print("Largest server type is " + largestType + ", there are " + largestServersList.size() + " " + largestType
+                + " servers.");
     };
 
-    public void updateCurrentJob(){
+    public void updateCurrentJob() {
         currentJob = new Job();
         String[] jobStringArray = getLatestMessageSplit();
         currentJob.submitTime = Integer.parseInt(jobStringArray[1]);
-        currentJob.jobID = Integer.parseInt(jobStringArray[2]);  
-        currentJob.estRuntime = Integer.parseInt(jobStringArray[3]);  
-        currentJob.core = Integer.parseInt(jobStringArray[4]);  
-        currentJob.memory = Integer.parseInt(jobStringArray[5]);  
-        currentJob.disk = Integer.parseInt(jobStringArray[6]);  
+        currentJob.jobID = Integer.parseInt(jobStringArray[2]);
+        currentJob.estRuntime = Integer.parseInt(jobStringArray[3]);
+        currentJob.core = Integer.parseInt(jobStringArray[4]);
+        currentJob.memory = Integer.parseInt(jobStringArray[5]);
+        currentJob.disk = Integer.parseInt(jobStringArray[6]);
     }
 
-    public void updateAllServersList(){
+    public void updateAllServersList() {
         allServersList = new ArrayList<Server>();
-        for(int i = 0; i < numberOfMessages; i++){
+        for (int i = 0; i < numberOfMessages; i++) {
             String[] serverStringArray = getMessageFromEndSplit(i);
             Server newServer = new Server();
             newServer.serverType = serverStringArray[0];
@@ -146,10 +156,10 @@ class MyClient {
             allServersList.add(0, newServer);
         }
     }
-    
-    public Server getCurrentLargestServer(){
+
+    public Server getCurrentLargestServer() {
         Server result = largestServersList.get(currentServerIndex);
-        if(currentServerIndex == largestServersList.size()-1){
+        if (currentServerIndex == largestServersList.size() - 1) {
             currentServerIndex = 0;
         } else {
             currentServerIndex++;
@@ -157,46 +167,55 @@ class MyClient {
         return result;
     };
 
-    public String getLatestMessage(){
-        return serverMessageList.get(serverMessageList.size()-1);
+    public String getLatestMessage() {
+        return serverMessageList.get(serverMessageList.size() - 1);
     }
 
-    public String[] getLatestMessageSplit(){
+    public String[] getLatestMessageSplit() {
         return getLatestMessage().split(" ");
     }
-    public String getMessageFromEnd(int offset){
-        return serverMessageList.get(serverMessageList.size()-(1+offset));
+
+    public String getMessageFromEnd(int offset) {
+        return serverMessageList.get(serverMessageList.size() - (1 + offset));
     }
 
-    public String[] getMessageFromEndSplit(int offset){
+    public String[] getMessageFromEndSplit(int offset) {
         return getMessageFromEnd(offset).split(" ");
     }
 
-    //this method writes a messages to an output stream then reads from the server back
-    public void writeThenRead(String message){
-        try{
+    // this method writes a messages to an output stream then reads from the server
+    // back
+    public void writeThenRead(String message) {
+        try {
             numberOfMessages = 0;
-            System.out.println("Client says: " + message);
+            print("Client says: " + message);
             dataOutputStream.write(message.concat("\n").getBytes());
             dataOutputStream.flush();
 
-            while(! reader.ready()){
+            while (!reader.ready()) {
                 continue;
             }
 
-            while(reader.ready()){
+            while (reader.ready()) {
                 numberOfMessages++;
                 String serverResponse = reader.readLine();
                 serverMessageList.add(serverResponse);
-                System.out.println("Server says: " + serverResponse);
+                print("Server says: " + serverResponse);
             }
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
-        };
+        }
+        ;
     };
 
+    public void print(Object message) {
+        if (printAllMessages) {
+            System.out.println(message);
+        }
+    }
+
 };
+
 class Job {
     int submitTime;
     int jobID;
